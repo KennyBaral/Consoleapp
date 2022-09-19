254455e566e86f4fc6f5e624aef00e46939926dc
kenny baral <94068798+KennyBaral@users.noreply.github.com>
2022-09-19 09:49:20 +0545

    first commit

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Hello
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine("how do i do this damn");
+        }
+
+    }
+}
